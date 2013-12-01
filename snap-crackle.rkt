75eab95a0c779913f77e3dfd03bbc35c664c8b5f
ben rudgers <ben.rudgers@gmail.com>
2013-12-01 16:18:52 -0600

    Version for Kludgecode blog
    Added validation for input values.
    Reformatted comments to form hierarchy.
    Output changed to "CracklePop"
    More recent than version submitted with HackerSchool application

--- snap-crackle.rkt
+++ snap-crackle.rkt
@@ -18,19 +18,39 @@
 ;;;    + the string which a buzz should return
 ;;; the third argument is the lower bound of the fizzbuzz's range
 ;;; the fourth argument is the upper bound of the fizzbuzz's range
-(define (fizz-buzzer pr1 pr2 start stop)
-  (let* ([iterations (range start (+ stop 1))]  ; 'range output not inclusive
+(define (fizz-buzzer fizz-pair buzz-pair start stop)
+  (let* (;; (int . string) -> (int . string) | raises error
+         ;; validates 'fizz-pair and 'buzz-pair
+         [validate-pair 
+          (lambda (pr name)
+            (let ([int (integer? (car pr))]
+                  [string (string? (cdr pr))])
+              (if (not int)
+                  (error 'fizz-buzzer "first element of ~a cannot be coerced to an integer" name)
+                  (if (not string)
+                      (error 'fizz-buzzer "second element of ~a not a string" name)
+                      pr))))]
+         ;; integer -> integer | raises error
+         ;; validates 'start and 'stop
+         [validate-int
+          (lambda (i name)
+            (if (integer? i)
+                i
+                (error 'fizz-buzzer " ~a value cannot be coerced to an integer" name)))]
          ;; note that Racket documentation uses
          ;; 'car and 'cdr for dotted pair examples
          ;; so following it here [see Racket Guide 3.8]
-         [fizz-val  (car pr1)]
-         [fizz-word (cdr pr1)]
-         [buzz-val  (car pr2)]
-         [buzz-word (cdr pr2)]
+         [fizz-val  (car (validate-pair fizz-pair "fizz-pair"))]
+         [fizz-word (cdr fizz-pair)]
+         [buzz-val  (car (validate-pair buzz-pair "buzz-pair"))]
+         [buzz-word (cdr buzz-pair)]
          ;; implicit fizzbuzz pair
          [fizzbuzz-val (* fizz-val buzz-val)]
          [fizzbuzz-word (string-append
                          fizz-word buzz-word)]
+         ;; generate the range of integers over which to iterate
+         [iterations (range (validate-int start "start")
+                            (+ (validate-int stop "stop") 1))]  ; 'range output not inclusive
          ;; int -> FizzBuzz
          ;; a helper for our map function
          [aux (lambda(int)
@@ -46,13 +66,13 @@
     (map aux iterations)))
 
 
-;; list -> side effect
-;; prints the contents of a list without 
-;; the enclosing parenthesis
+;;; list -> side effect
+;;; prints the contents of a list without 
+;;; the enclosing parenthesis
 (define (print-list lst)
   (letrec 
-       ;; any -> string
-       ;; string ends with a newline character
+      ;; any -> string
+      ;; string ends with a newline character
       ([formatter (lambda (x) (format "~a ~n" x))]
        ;; listof any -> string
        [aux (lambda (lst)
@@ -63,11 +83,11 @@
     (display (aux lst))))
 
 
-;; -> side effect
-;; a classic fizzbuzz using "snap" and "crackle"
-(define snap-crackle 
-  (lambda ()(print-list(fizz-buzzer '(3 . "snap")
-                                    '(5 . "crackle") 
+;;; -> side effect
+;;; a classic fizzbuzz using "Crackle" and "Pop"
+(define crackle-pop
+  (lambda ()(print-list(fizz-buzzer '(3 . "Crackle")
+                                    '(5 . "Pop") 
                                     1 100))))
 
-(snap-crackle)
+(crackle-pop)
